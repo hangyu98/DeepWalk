e2d59675a11a7df84d5c3471572de1cf0d359217
Izzy Gao <izzygao@Izzy-Gao-3.local>
2021-11-29 21:10:41 -0500

    fix classification report

--- classification.py
+++ classification.py
@@ -3,29 +3,38 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 
+
 def load_x(embeddings_dict):
     x = []
     for key in embeddings_dict:
-        x = x.append(embeddings_dict[key])
+        x.append(embeddings_dict[key])
     return x
-    
+
+
 def load_y(embeddings_dict, node_labels_dict):
     y = []
     for key in embeddings_dict:
-        y = y.append(node_labels_dict[key])
+        y.append(node_labels_dict[key])
     return y
-    
+
+
 def split(x, y):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42, shuffle=True)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.20, random_state=42, shuffle=True)
     return x_train, x_test, y_train, y_test
+
 
 def train(x, y):
     classifier = MLPClassifier()
     classifier.fit(x, y)
+    return classifier
+
 
 def test(classifier, x, y_true):
     y_pred = classifier.predict(x)
-    return classification_report(y_true, y_pred,labels=[1, 2, 3, 4])
+    # return classification_report(y_true, y_pred, labels=['politician', 'company', 'government', 'tvshow'])
+    return classification_report(y_true, y_pred, target_names=['politician', 'company', 'government', 'tvshow'])
+
 
 def classify(embeddings_dict, node_labels_dict):
     x = load_x(embeddings_dict)
--- word2vec.py
+++ word2vec.py
@@ -1,10 +1,12 @@
 from gensim.models import Word2Vec
 
+
 def generate_emb(G, random_walks, window_size, emb_size):
     word2vec = Word2Vec(random_walks,
                         window=window_size, sg=1)
     embeddings = {}
     for n in G.nodes():
         embeddings[n] = word2vec.wv[n]
-    
+    # print("embeddings:", embeddings['1'])
+
     return embeddings
